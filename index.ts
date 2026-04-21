8220ecb385fed02ffead7a8982aff584f309d89a
Dantar2009 <dantar9@yandex.ru>
2026-04-21 16:42:49 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -22,9 +22,7 @@ const limiter = rateLimit({
 app.use(limiter)
 
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173"
-}))
+app.use(cors())
 app.use(express.json())
 
 
